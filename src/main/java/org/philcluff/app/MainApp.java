a3a0a62f24c8e4c76403f8f42158c28fc2e570d2
Phil Cluff <philcluff@googlemail.com>
2014-02-15 00:11:52 +0000

    Change pom to match new structure

--- src/main/java/org/philcluff/app/MainApp.java
+++ src/main/java/org/philcluff/app/MainApp.java
@@ -2,14 +2,10 @@ package org.philcluff.app;
 
 import org.apache.camel.main.Main;
 import org.philcluff.route.SimpleRouteBuilder;
-/**
- * A Camel Application
- */
+
 public class MainApp {
 
-    /**
-     * A main() so we can easily run these routing rules in our IDE
-     */
+
     public static void main(String... args) throws Exception {
         Main main = new Main();
         main.enableHangupSupport();
