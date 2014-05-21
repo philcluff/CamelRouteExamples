399a7ed3ec3e41ecefc94c623ac014b73a145cc3
Phil Cluff <pcluff@brightcove.com>
2014-05-20 17:44:54 -0700

    Wire up a camel context by hand. Supports SQS and file at the moment.

--- src/main/java/org/philcluff/app/MainApp.java
+++ src/main/java/org/philcluff/app/MainApp.java
@@ -1,13 +1,67 @@
 package org.philcluff.app;
 
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSClient;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Component;
+import org.apache.camel.Endpoint;
+import org.apache.camel.component.aws.sqs.SqsComponent;
+import org.apache.camel.component.file.FileComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
+import org.philcluff.route.SimpleRouteBuilder;
+
 public class MainApp {
 
+    private final static String SQS_CLIENT_NAME = "sqsClient";
+    private final static String SQS_ENDPOINT_PARAMS = "?amazonSQSClient=#" + SQS_CLIENT_NAME;
+    private final static Region SQS_REGION = Region.getRegion(Regions.EU_WEST_1);
 
     public static void main(String... args) throws Exception {
 
-        // TODO... An App here that doesn't depend on Spring?
-        // HA!
+        // "aws-sqs://a" OR "file:///tmp/camel/a/"
+        String inputEndpointUri = "aws-sqs://a";
+        // "aws-sqs://b" OR "file:///tmp/camel/b/"
+        String outputEndpointUri = "file:///tmp/camel/b/";
 
+        SimpleRegistry reg = setupCamelRegistryForSQS();
+        CamelContext context = getCamelContext(reg);
+
+        Endpoint inputEndpoint = getEndpointByUri(inputEndpointUri, context);
+        Endpoint outputEndpoint = getEndpointByUri(outputEndpointUri, context);
+
+        context.addRoutes(new SimpleRouteBuilder(inputEndpoint, outputEndpoint));
+        context.start();
+        Thread.currentThread().join();
+
+    }
+
+    private static SimpleRegistry setupCamelRegistryForSQS() {
+        AmazonSQSClient client = new AmazonSQSClient();
+        client.setRegion(SQS_REGION);
+        SimpleRegistry reg = new SimpleRegistry();
+        reg.put(SQS_CLIENT_NAME, client);
+        return reg;
+    }
+
+    private static CamelContext getCamelContext(SimpleRegistry reg) {
+        CamelContext context = new DefaultCamelContext(reg);
+        Component fileComponent = new FileComponent();
+        Component sqsComponent = new SqsComponent();
+        fileComponent.setCamelContext(context);
+        sqsComponent.setCamelContext(context);
+        return context;
+    }
+
+    private static Endpoint getEndpointByUri(String uri, CamelContext context) throws Exception {
+        if (uri.startsWith("aws-sqs")) {
+            return context.getComponent("aws-sqs").createEndpoint(uri + SQS_ENDPOINT_PARAMS);
+        }
+        else if (uri.startsWith("file")) {
+            return context.getComponent("file").createEndpoint(uri);
+        }
+        return null;
     }
 
 }
--- src/main/java/org/philcluff/route/SimpleRouteBuilder.java
+++ src/main/java/org/philcluff/route/SimpleRouteBuilder.java
@@ -2,9 +2,18 @@ package org.philcluff.route;
 
 import org.apache.camel.Endpoint;
 import org.apache.camel.EndpointInject;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 
 public class SimpleRouteBuilder extends RouteBuilder {
+
+    public SimpleRouteBuilder(Endpoint in, Endpoint out) {
+        this.in = in;
+        this.out = out;
+    }
+
+    public SimpleRouteBuilder() {
+    }
 
     @EndpointInject(ref="endpoint-in") protected Endpoint in;
     @EndpointInject(ref="endpoint-out") protected Endpoint out;
@@ -12,6 +21,7 @@ public class SimpleRouteBuilder extends RouteBuilder {
     // Simplest route builder - Consume from one endpoint, and produce to a different one.
     public void configure() {
         from(in)
+                .log(LoggingLevel.WARN, "A message has been processed.")
             .to(out);
     }
 
